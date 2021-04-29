fd44697d08cf28c8b9253950eb4aa5f684169e7e
Sajith <sajith.kumar@servcorp.com.au>
2021-04-29 13:03:23 +1000

    Updated Program.cs

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -6,7 +6,25 @@ namespace Exercise2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FileReverse fileRev = new FileReverse();
+            Console.WriteLine("Enter a file name : ");
+            string fileName = Console.ReadLine();
+            Console.WriteLine(" ----- ORIGINAL CONTENTS -----");
+            string[] fileContent = fileRev.OpenFile(fileName);
+            foreach (string line in fileContent)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine("\n ----- REVERSED CONTENTS -----");
+            string[] revFileContents = fileRev.ReverseContents(fileContent);
+            foreach (string line in revFileContents)
+            {
+                Console.WriteLine(line);
+            }
+
+            fileRev.SaveFile(fileName, revFileContents);
+            Console.WriteLine($"\n Reversed contents saved to file : {fileName}");
+            Console.ReadLine();
         }
     }
 }
