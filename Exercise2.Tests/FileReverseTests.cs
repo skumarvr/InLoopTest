2e0ec8fc3858663efa181f9dd3be8e5492d16fff
Sajith <sajith.kumar@servcorp.com.au>
2021-04-29 11:56:43 +1000

    Added Basic Tests

---
+++ Exercise2.Tests/FileReverseTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+namespace Exercise2.Tests
+{
+    public class FileReverseTests
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+
+        }
+
+        [Test]
+        public void Test_ReverseContents()
+        {
+            FileReverse fr = new FileReverse();
+
+            string[] input = null;
+
+            var output = fr.ReverseContents(input);
+            Assert.AreEqual(input, output);
+
+            input = new string[] { "A" };
+
+            var output1 = fr.ReverseContents(input);
+            Assert.AreEqual(input, output1);
+
+            input = new string[] { "QWERT" };
+
+            var output2 = fr.ReverseContents(input);
+            Assert.AreEqual(new string[] { "TREWQ" }, output2);
+
+            input = new string[] { "QWERT", "ASDFG" };
+            var output3 = fr.ReverseContents(input);
+            Assert.AreEqual(new string[] { "GFDSA", "TREWQ"}, output3);
+
+            input = new string[] { "QWERT", "ASDFG", "ZXCVB" };
+            var output4 = fr.ReverseContents(input);
+            Assert.AreEqual(new string[] { "BVCXZ", "GFDSA", "TREWQ" }, output4);
+        }
+    }
+}
